4ddd5108cf4e23a21dfda5be2c466c95141b62be
Lacuna543 <kusunya@i.ua>
2020-06-12 21:36:16 +0300

    Correct naming by convention

---
+++ src/task2/Employee.java
@@ -0,0 +1,26 @@
+package task2;
+
+public class Employee {
+    public String fullName;
+    public float salary;
+
+    public static void main(String[] args) {
+
+        String employeesInfo = "[";
+
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        emp1.fullName = "Garry Petrovsky";
+        emp2.fullName = "Bob Roork";
+
+        emp1.salary = 10000.6f;
+        emp2.salary = 9000.6f;
+
+        Employee[] employees = {emp1, emp2};
+
+        for (Employee employee : employees) {
+            employeesInfo = employeesInfo.concat("{fullName: \"" + employee.fullName + "\", salary: " + employee.salary + "}, " + "]");
+        }
+
+    }
+}
