6798c8f87b701cd1b4aece56578c252d716bc696
Lacuna543 <kusunya@i.ua>
2020-06-26 17:23:31 +0300

    Added Sprint05.task5 (should be reworked)

--- src/Sprint05/task4/Person.java
+++ src/Sprint05/task4/Person.java
@@ -16,20 +16,21 @@ public class Person {
         }
     }
 
-    public void setLastName(String firstName) {
+    public void setLastName(String lastName) {
         if (lastName.matches("[A-Z][\\w- ]+")) {
-            this.lastName = firstName;
+            this.lastName = lastName;
         } else if (this.lastName == null) {
             throw new NameException("Incorrect value " + lastName + " for lastName" +
                     " (should start from upper case and contains only alphabetic characters and symbols -, _); ");
         }
     }
 
-    public void setIdCode(String idCode) throws CodeException {
-        if (!idCode.matches("\\d{10}")) {
-            throw new CodeException("");
+    public void setIdCode(String idCode) {
+        if (idCode.matches("\\d{10}")) {
+            this.idCode = idCode;
+        } else {
+            throw new CodeException("Incorrect value " + idCode + " for code (should contains exactly 10 digits)");
         }
-        this.idCode = idCode;
     }
 
 
@@ -50,29 +51,31 @@ public class Person {
 
     @Override
     public String toString() {
-        return ": " + firstName + " " + lastName + ": " + idCode;
+        return  firstName + " " + lastName + ": " + idCode;
     }
 
     public static Person buildPerson(String firstName, String lastName, String idCode) {
         Person person = new Person();
         String message = "";
-        try{
+        try {
             person.setFirstName(firstName);
             person.setLastName(lastName);
-        }catch (NameException nameException){
-            message +=nameException.toString();
+        } catch (NameException nameException) {
+            message += nameException.getMessage();
         }
+
         try {
             person.setIdCode(idCode);
-        }catch (CodeException codeException){
-            message +=codeException.toString();
+        } catch (CodeException codeException) {
+            message += codeException.getMessage();
         }
         if (message.isEmpty()) {
             return person;
         }
         throw new IllegalArgumentException(message);
-    }
 
+
+    }
 }
 
 class NameException extends RuntimeException {
