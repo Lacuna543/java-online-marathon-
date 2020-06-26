ca0cf6bd7e908782f2688f71686f66313ee8a227
Lacuna543 <kusunya@i.ua>
2020-06-26 11:21:38 +0300

    Added comment to Sprint05.task1 and solution Sprint05.task1 and Sprint05.task3

--- src/Sprint05/task1/Operation.java
+++ src/Sprint05/task1/Operation.java
@@ -1,10 +1,11 @@
 package Sprint05.task1;
 
 class Operation {
-    public static int squareRectangle(int a, int b) throws IllegalArgumentException {
+    public static int squareRectangle(int a, int b) throws IllegalArgumentException { //этот метод возможно выбросит такое исключение, в ходе метода может возникнуть ошибка, кр текст, таким образом обраюат. исключ. (контролируем)
+
 
         if (a <= 0 || b <= 0) {
-            throw new IllegalArgumentException("both arguments should be more than zero");
+            throw new IllegalArgumentException("both arguments should be more than zero");//если мы считаем, что программа рабоатет ошибочноЮ если пришли не в то состояние, которое должны, то выбрасывает исключ
         }
                 return a * b;
 
@@ -12,9 +13,9 @@ class Operation {
 
     public static int trySquareRectangle(int a, int b) {
 
-        try {
+        try { //пытается вызвать эту часть кода и надеемся что искл. не будкет выброшено
             return squareRectangle(a, b);
-        }catch (IllegalArgumentException illegalArgumentException){
+        }catch (IllegalArgumentException illegalArgumentException){ //но если вырошено, обрабатываем тут
             illegalArgumentException.getMessage();
         }
         return -1;
---
+++ src/Sprint05/task3/Plant.java
@@ -0,0 +1,81 @@
+package Sprint05.task3;
+
+enum Color {
+    WHITE, RED, BLUE
+}
+
+enum Type {
+    RARE, ORDINARY
+}
+
+public class Plant {
+    private String name;
+    private Color color;
+    private Type type;
+
+    public Plant(String type, String color, String name) throws ColorException, TypeException {
+        this.name = name;
+
+        try {
+            this.type = Type.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException argumentException) {
+            throw new TypeException("Invalid value " + type + " for field type");
+        }
+
+        try {
+            this.color = Color.valueOf(color.toUpperCase());
+        } catch (IllegalArgumentException argumentException) {
+            throw new ColorException("Invalid value " + color + " for field color");
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "{type: " + type +
+                ", color: "
+                + color +
+                ", name: " + name + //"MyPlant"
+                '}';
+    }
+
+    public static Plant tryCreatePlant(String type, String color, String name) throws TypeException, ColorException {
+        Plant plant;
+        try {
+            plant = new Plant(type, color, name);
+        } catch (TypeException typeException) {
+           plant = tryCreatePlant("Ordinary", color, name);
+
+        }catch (ColorException colorException) {
+         plant = tryCreatePlant(type, "Red", name);
+        }
+        return plant;
+    }
+
+}
+
+
+class ColorException extends Exception {
+    public ColorException(String message) {
+        super(message);
+    }
+}
+
+class TypeException extends Exception {
+    public TypeException(String message) {
+        super(message);
+    }
+}
+
